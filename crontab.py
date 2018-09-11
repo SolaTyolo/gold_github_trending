31afcf058091ab331ff738bbf9f61f3c3c49ce29
SolaTyolo <solat.yolo@gmail.com>
2018-09-11 09:57:02 +0800

    remove chinese

--- crontab.py
+++ crontab.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import http.client
 import json
 import time
@@ -11,7 +13,6 @@ def gitCmd(date):
     gitCommit = 'git commit -m ' + date
     gitPush = 'git push -u origin master'
 
-    # 执行shell
     os.system(gitAdd)
     os.system(gitCommit)
     os.system(gitPush)
@@ -24,19 +25,12 @@ def createMarkdown(date,filename,lang):
 def writeMarkdown(data,filename):
     with open(filename,"a") as f:
         for value in data:
-            #项目描述
             description = value['description']
-            #仓库链接
             url = value['url']
-            #项目名称
             title = value['reponame']
-            #作者名字
             username = value['username']
-            #start数
             starCount = value['starCount']
-            #fork数
             forkCount = value['forkCount']
-            #头像
             avatar = value['owner']['avatar']
             
             content = '''
@@ -51,7 +45,6 @@ def writeMarkdown(data,filename):
 
 '''.format(title=title, url=url, description=description,username=username,starCount=starCount,forkCount=forkCount)
 
-            #写入文件
             f.write(content)
 
 
@@ -84,11 +77,9 @@ def scrapy(lang):
 
 def crontab():
     strdate = datetime.datetime.now()
-    # 获取星期数
     weekday = strdate.weekday()
     date = strdate.strftime('%Y-%m-%d')
 
-    #根据星期来获取语言
     lang = ""
 
     if weekday == 1 :
@@ -109,16 +100,11 @@ def crontab():
         sys.exit(0)
 
     filename = './{lang}/{date}.md'.format(lang=lang,date=date)
-
-    # 创建markdown文件
     createMarkdown(date,filename,lang)
-    # 获取trending
     data = scrapy(lang)
-    # 写入markdown文件
     data = json.loads(data)
     if data['code'] == 200:
         writeMarkdown(data['data'],filename)
-        # git提交
         gitCmd(date)
 
 if __name__ == '__main__':
